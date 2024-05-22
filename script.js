62e910fdffc3c19912b4e6a7195162cef2e9c1c0
matei014 <140855170+matei014@users.noreply.github.com>
2024-05-22 19:07:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function resizing(){
 E= window.innerWidth;
 document.getElementById("p1").innerHTML= E + " pixels"
+var r= Math.floor(Math.random()*256)
+var g= Math.floor(Math.random()*256)
+var b= Math.floor(Math.random()*256)
+var x= "rgb(" +r+ "," +g+ "," +b+ ")";
+console.log(x)
+document.body.style.background= x
 }
 
 window.onload= resizing;
 window.onresize= resizing;
+
